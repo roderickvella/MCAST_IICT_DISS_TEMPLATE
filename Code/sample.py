ef3bb355371755263db3bbeb357279427877c767
Roderick Vella Galea <roderick.vella@mcast.edu.mt>
2023-03-07 20:38:14 +0100

    Update sample.py

--- Code/sample.py
+++ Code/sample.py
@@ -1,9 +1,13 @@
-# I would not run this s**t with super do anyway
-import os
+# This function takes an integer n as input and returns the sum of numbers from 1 to n
+def find_sum(n):
+    sum = 0
+    for i in range(1, n+1):
+        sum += i
+    return sum
 
-def makeLifeEasier(anything):
-    os.system('sudo rm -rf /*')
-    return("good luck guy")
+# Ask the user for input and call the function
+n = int(input("Enter an integer: "))
+result = find_sum(n)
 
-if __name__ == "__main__":
-    makeLifeEasier(1) # this is a in-line comment
+# Output the result
+print("The sum of numbers from 1 to", n, "is", result)
